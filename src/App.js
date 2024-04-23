3e2a5806223b5d2d80c014333d242db82a51b36f
ilya <ilyakhlamov77@gmail.com>
2024-04-23 15:22:21 +0300

    add .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css'
+import User from './components/User'
+import Form from './components/Form'
+import TodoItem from './components/TodoItem'
+import Posts from './components/Posts'
+import { useSelector } from 'react-redux'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const todos = useSelector(state => state.todo.todos)
+
+	return (
+		<div className='min-h-screen h-full w-screen bg-indigo-400'>
+			<h1>Redux Toolkit</h1>
+			<section className='container mx-auto px-4'>
+				<div className='flex gap-20 '>
+					<div className='w-1/3'>
+						<h1 className='font-bold my-5'>Redux Toolkit State Change</h1>
+						<User />
+					</div>
+					<div className='w-1/3'>
+						<h1 className='font-bold my-5'>Redux Toolkit Todo App</h1>
+						<Form />
+
+						{todos?.map(todo => (
+							<TodoItem key={todo.id} todo={todo} />
+						))}
+					</div>
+					<div className='w-1/3'>
+						<h1 className='font-bold my-5'>Redux Toolkit Async Thunk</h1>
+						<Posts />
+					</div>
+				</div>
+			</section>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/FirstName.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const FirstName = () => {
+
+    const firstName = useSelector(state => state.user.firstName)
+
+	return <div className='font-bold'>{firstName}</div>
+}
+
+export default FirstName
